11295b15ac16d98446de6707423590fff15130d3
Gitguy22 <113185690+Gitguy22@users.noreply.github.com>
2022-10-13 08:51:22 -0400

    part 1-3 done

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,14 +7,21 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
     }
 
     // Update is called once per frame
     void Update()
     {
+      float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+
+        // Declared position variable and set it equal to players position
        Vector2 position = transform.position;
-       position.x = position.x + 0.1f;
+       // Update players x position .1 unit every frame
+       position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+       position.y = position.y + 3.0f * vertical * Time.deltaTime;
        transform.position = position;   
     }
 }
