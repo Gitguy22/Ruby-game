ee73b44071bf253b57a11ccef64fc15b9610e1ef
Gitguy22 <113185690+Gitguy22@users.noreply.github.com>
2023-02-08 09:02:59 -0500

    Woopwoop

--- Assets/Scripts/UIHealthBar.cs
+++ Assets/Scripts/UIHealthBar.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static UIHealthBar instance { get; private set }
+    public Image mask;
+    float originalSize;
+
+    //called when object it created.
+    void Awake()
+    {
+        instance = this;
+    }
+    
     void Start()
     {
-        
+        originalSize = mask.rectTransform.rect.width;
     }
 
-    // Update is called once per frame
+    //
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+    
     void Update()
     {
         
