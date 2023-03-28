f6b95d9438b00180104d387acffac6b4ee931a2b
Gitguy22 <113185690+Gitguy22@users.noreply.github.com>
2023-03-28 17:11:55 -0400

    Final Push

--- Assets/Scripts/RedBerry.cs
+++ Assets/Scripts/RedBerry.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class RedBerry : MonoBehaviour
 {
 public GameObject blueBerry;
+Dialogue dialogue;
+
+void Start()
+
+{
+    dialogue = FindObjectOfType<Dialogue>();
+}
 
  void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(gameObject);
         Destroy(blueBerry);
+        Destroy(dialogue.dialogBox);
     }
 }
